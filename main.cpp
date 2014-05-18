337a3a85c64d2494ded70562713141a8a09515f7
Pavel Sukhov <pasukhov@yandex-team.ru>
2014-05-18 22:49:36 +0400

    LADS

--- main.cpp
+++ main.cpp
@@ -1,68 +1,119 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
-#include<set>
-#include<iterator>
-
-using namespace std;
-
-typedef vector<int> vi;
-typedef vector<vector<int> > vvi;
-
-void getNeighbourSwap(vi permutation, vvi& neibhours){
-	int n = permutation.size();
-	
-	vi::iterator end = permutation.end();
-	vi::iterator it = permutation.begin();
-	++it;
-	--end;
-	for(; it != end; ++it)
-		for(vi::iterator jt = it; ++jt != end; ){
-			swap(*it,*jt);
-			neibhours.push_back(permutation);
-			swap(*it,*jt);
-		}
-}
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <limits>
+#include <iterator>
+#include <map>
+#include <sstream>
+#include <ctime>
 
 
-long CalculatePatchLength(vvi matrix, vi permutation, vi path){
-	long res = 0;
-	vi::iterator it = path.begin();
-	vi::iterator jt = it;
-	++jt;
-	vi::iterator end = path.end();
-	for(; jt != end; ++it,++jt){
-		res += matrix[permutation[*it]][permutation[*jt]];
-	}
-	return res;
-}
+namespace LADS {
+/*
+    template <class SizeClass>
+    struct Column{
+        Column(SizeClass i){ index = i;};
+        Column<SizeClass>& operator=(Column<SizeClass>& a){ index = a.index; return *this;};
+        SizeClass index;
+    };
+*/
+    template <class BaseClass, class SizeClass>
+    class Row{
+        std::vector<SizeClass >* columnPermutation;
+        BaseClass* base;
+        public:
+        SizeClass index;
+        Row(): base(0), columnPermutation(0) {
+        }
+        Row(BaseClass* base, std::vector<SizeClass >*  columnPermutation, SizeClass index ){
+            this->index = index;
+            this->base = base;
+            this->columnPermutation = columnPermutation;
+        }
+        BaseClass& operator[](SizeClass i){
+            return base[ columnPermutation -> at(i) ];
+            //return base[ (*columnPermutation)[i] ];
+        }
+    };
 
 
+    template<class DataClass, class SizeClass>
+    class Matrix {
 
-template<typename Func,typename Uns>
-void (vvi matrix,vi permutation,Func f );
+        DataClass** matrix;
+        std::vector<SizeClass > columns;
+        std::vector<Row<DataClass , SizeClass > > rows;
+        public:
+        typedef typename std::vector<Row<DataClass ,  SizeClass > >::iterator RowIterator;
+        typedef typename std::vector< SizeClass  >::iterator ColumnIterator;
+        RowIterator rowBegin(){
+            return rows.begin();
+        }
+        RowIterator rowEnd(){
+            return rows.end();
+        }
+        ColumnIterator columnBegin(){
+            return columns.begin();
+        }
+        ColumnIterator columnEnd(){
+            return columns.end();
+        }
+
+        Row<DataClass , SizeClass >& operator[](SizeClass i){
+            //return rows.at(i);
+            return rows[i];
+        }
+
+        Matrix(DataClass** a, SizeClass n, SizeClass m) : matrix(a)/*, columns(m), rows(n)*/{
+            for(SizeClass i = 0; i < m; ++i) columns.push_back( i );
+            for(SizeClass i = 0; i < n; ++i){
+                Row<DataClass, SizeClass > row(matrix[i], &columns, i);
+                rows.push_back(row);
+            }
+        }
+
+        void swap_rows(SizeClass a, SizeClass b){
+            std::swap(rows.at(a), rows.at(b));
+        }
+        void swap_columns(SizeClass a, SizeClass b){
+            std::swap(columns.at(a), columns.at(b));
+        }
+
+        void setRowPermutation(std::vector<SizeClass> permutation ){
+            std::vector< Row<DataClass , SizeClass > > temp (rows.size()) ;
+            std::copy(rows.begin(), rows.end(), temp.begin());
+            RowIterator res = rows.begin();
+            typename std::vector<SizeClass>::iterator it = permutation.begin();
+            typename std::vector<SizeClass>::iterator end = permutation.end();
+            for(; it != end; ++it, ++res) *res = temp.at(*it);
+        }
+
+        void setColumnPermutation(std::vector<SizeClass> permutation ){
+            std::vector< SizeClass > temp (columns.size()) ;
+            std::copy(columns.begin(), columns.end(), temp.begin());
+            ColumnIterator res = rows.begin();
+            typename std::vector<SizeClass>::iterator it = permutation.begin();
+            typename std::vector<SizeClass>::iterator end = permutation.end();
+            for(; it != end; ++it, ++res) *res = temp.at(*it);
+        }
+
+        DataClass** getInitialMatrix(){
+            return matrix;
+        }
+
+    };
+};
+
+
+typedef LADS::Matrix<int,int> Mi;
+typedef LADS::Matrix<double,int> Md;
+
 
 
 
 int main(){
-	/*
-	vi v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
 
-	vvi rv;
 
-	getNeighbourSwap(v,rv);
 
-	vvi::iterator end = rv.end();
-	for (vvi::iterator it = rv.begin(); it != end; ++it){
-		copy(it->begin(), it->end(), ostream_iterator<int>(cout," "));
-		cout << endl;
-	}
-	*/
-
-	cin.get();
 }
